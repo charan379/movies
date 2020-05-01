ca5cfa4b71a4b7d8c0eb62acc42a21767ff1ede2
charanteja379 <charanteja379@users.noreply.github.com>
2020-05-01 08:31:21 +0530

    made Installation a little bit simple

--- export-to-xls/db_connect.php
+++ export-to-xls/db_connect.php
@@ -1,9 +1,11 @@
 <?php
+define('DIRECTACCESS', TRUE);
+require_once('../config/config.php');
 /* Database connection start */
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mymc";
+$servername = $settings["db"]["host"];
+$username = $settings["db"]["user"] ;
+$password = $settings["db"]["pass"] ;
+$dbname = $settings["db"]["name"];
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
