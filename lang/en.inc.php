d10feba0970e3f9c4af6e742bf7685441456e88e
charan379 <charanteja379@gmail.com>
2020-05-30 17:36:55 +0530

    openloadmovies.net streaming and google recaptcha support for login page is added

--- lang/en.inc.php
+++ lang/en.inc.php
@@ -105,8 +105,9 @@ define("LOANED_OUT",								"Loaned out");
 define("TO",										"to");
 define("ON",										"on");
 define("DOWNLOAD_PAGE_HEAD",            "Download Page");
-define("DOWNLOAD_MOVIE_SECTION",        "Download Links");
+define("DOWNLOAD_MOVIE_SECTION",        "Download & Streaming Links");
 define("GO_TO_TORRENTS",            "Go To Torrents");
+define("GO_TO_STREAM",               "Watch Online");
 
 /**
 * Add/edit movie
