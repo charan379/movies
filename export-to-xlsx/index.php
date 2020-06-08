c1a07c38550bf300a2e18a6b687be3a143169a8b
charan379 <charanteja379@gmail.com>
2020-06-08 20:11:11 +0530

    export movies table to xlsx sheet

---
+++ export-to-xlsx/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once("db-class.php");
+require_once("xlsxwriter.class.php");
+ini_set("display_errors", 1);
+ini_set("log_errors", 1);
+error_reporting(E_ALL & ~E_NOTICE);
+
+// Get Data From SQL DataBase;
+function getData() {
+	$db = new MY_SQLDB();
+	$sql = "SELECT * FROM `movies`";
+	$rows = $db->get_rows($sql);
+	$sheet_titles = $db->get_column_names();
+	$data = array_merge(array(), $rows);
+	array_unshift($data , $sheet_titles);
+	$db->close_connection();
+	return $data;
+}
+
+$data = getData();
+$filename = "movies.xlsx";
+
+$writer = new XLSXWriter();
+$writer->writeSheet($data);
+$writer->writeToFile($filename);
+
+
+header('Content-disposition: attachment; filename="'.XLSXWriter::sanitize_filename($filename).'"');
+header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+header('Content-Transfer-Encoding: binary');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+readfile($filename);
+exit(0);
+
+?>
